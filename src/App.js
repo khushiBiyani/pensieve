96958b1a1f89d0c1b1651bf8b714a0d7d74b4d0e
Chirag Jaju <f20201089@hyderabad.bits-pilani.ac.in>
2022-02-08 15:57:01 +0530

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import { useState, useContext } from "react";
 import { Button, Paper } from "@mui/material";
+import { Link, Outlet } from "react-router-dom";
 
 import "./App.css";
+
 import CustomTheme from "./components/theme/theme";
 import AuthContext from "./context/AuthContext";
 
-
 function App() {
   const [dark, setDark] = useState(true);
-  const {a, setA} = useContext(AuthContext);
+  const { a, setA } = useContext(AuthContext);
 
   return (
     <CustomTheme dark={dark}>
       <div className="App">
-
-      
         <Paper>
           <Button
             color="primary"
@@ -26,11 +25,12 @@ function App() {
           >
             Toggle dark mode
           </Button>
+          <Link to="/home">Home</Link> | <Link to="/about">About</Link> |{" "}
+          <Link to="/home/1998">1998 home</Link> |{" "}
+          <Link to="/home/2020">2020 home</Link>
         </Paper>
-
-
-
       </div>
+      <Outlet />
     </CustomTheme>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { AuthContextProvider } from "./context/AuthContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="home" element={<Home />}>
+              <Route path=":homeNumber" element={<Home />} />
+            </Route>
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
