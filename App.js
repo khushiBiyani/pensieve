2375229d89f984222303b77271e055cef08b4b50
Chirag Jaju <f20201089@hyderabad.bits-pilani.ac.in>
2022-02-23 01:47:26 +0530

    Mongo setup basic

---
+++ App.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000"],
+//     credentials: true,
+//   })
+// );
+mongoose.connect(process.env.MDB_CONNECT, {
+  useNewUrlParser: true,
+
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+app.use(express.json());
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+});
