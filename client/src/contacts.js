087a2181d2b38c06b7b7a17bb1884d09290b1d85
Chirag Jaju <f20201089@hyderabad.bits-pilani.ac.in>
2022-03-22 09:49:00 +0530

    Add Dummy Contacts

---
+++ client/src/contacts.js
@@ -0,0 +1,29 @@
+var contacts = [
+  {
+    Name: "Chirag Jaju",
+    Email: "f20201089@hyderabad.bits-pilani.ac.in",
+    Batch: 2020,
+  },
+  {
+    Name: "Khushi Biyani",
+    Email: "f20200194@hyderabad.bits-pilani.ac.in",
+    Batch: 2020,
+  },
+  {
+    Name: "Devarshi Sanghani",
+    Email: "f20202041@hyderabad.bits-pilani.ac.in",
+    Batch: 2020,
+  },
+  {
+    Name: "Tanya Prasad",
+    Email: "f20181054@hyderabad.bits-pilani.ac.in",
+    Batch: 2018,
+  },
+  {
+    Name: "Utkarsh Jain",
+    Email: "f20180456@hyderabad.bits-pilani.ac.in",
+    Batch: 2018,
+  },
+];
+
+export default contacts;
