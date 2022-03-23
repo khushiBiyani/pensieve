385cbeedae5d97a30fc0d9c57dee782a3c9ce6ac
khushiBiyani <93194800+khushiBiyani@users.noreply.github.com>
2022-03-23 12:32:47 +0530

    Card component base

--- client/src/pages/testForYou.js
+++ client/src/pages/testForYou.js
@@ -1,3 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "../components/TestimonialForYou/Card";
+import { Grid } from "@mui/material";
+
 export default function TestByYou() {
-  return <></>;
+  return (
+    <Grid
+      container
+      justify="center"
+      style={{ marginLeft: "300px", marginTop: "30px" }}
+    >
+      <Grid item p={2} xs={12} md={6}>
+        <Card />
+      </Grid>
+      <Grid item p={2} xs={12} md={6}>
+        <Card />
+      </Grid>
+      <Grid item p={2} xs={12} md={6}>
+        <Card />
+      </Grid>
+    </Grid>
+  );
 }
